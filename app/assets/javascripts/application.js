0227ba780033f11fa755f5c85c3a7ec7361495ad
CarlsBad505 <carl@tabrific.com>
2018-05-28 16:04:41 -0600

    skeleton in progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery.easing
+//= require jquery.magnific-popup.min
+//= require popper
+//= require bootstrap-sprockets
+//= require scrollreveal.min
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
